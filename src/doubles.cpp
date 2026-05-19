cfeea19d6312c09d314399b7f603400d6d1e6c16
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-05-19 10:13:43 +0100

    Migration from cpp11 to cppally

--- src/doubles.cpp
+++ src/doubles.cpp
@@ -1,8 +1,8 @@
-#include <cpp11.hpp>
-using namespace cpp11;
+#include <cppally.hpp>
+using namespace cppally;
 
 // Author: Nick Christofides
-// Date: 06-June-2025
+// Date: 19-May-2026
 // License: MIT License
 
 // Below are a complete set of C++ functions for comparing doubles
@@ -11,43 +11,32 @@ using namespace cpp11;
 // Relative differences are used except when either x or y is very close to zero
 // in which case absolute differences are used
 
-#ifndef DOUBLE_EPS
-#define DOUBLE_EPS std::numeric_limits<double>::epsilon()
-#endif
-
-#ifndef DEFAULT_TOL
-#define DEFAULT_TOL std::sqrt(std::numeric_limits<double>::epsilon())
-#endif
-
-inline bool is_na(double x){
-  return x != x;
+constexpr r_dbl default_tol(){
+  return r_limits<r_dbl>::tolerance();
 }
 
-inline bool is_inf(double x){
-  return std::fabs(x) == R_PosInf;
+r_lgl is_inf(r_dbl x){
+  return abs(x) == pos_inf;
 }
 
-inline bool both_same_inf(double x, double y){
-  return (x == R_PosInf && y == R_PosInf) || (x == R_NegInf && y == R_NegInf);
+r_lgl both_same_inf(r_dbl x, r_dbl y){
+  return (x == pos_inf && y == pos_inf) || (x == neg_inf && y == neg_inf);
 }
-inline bool any_inf(double x, double y){
+r_lgl any_inf(r_dbl x, r_dbl y){
   return is_inf(x) || is_inf(y);
 }
-inline double abs_diff(double x, double y){
-  return std::fabs(x - y);
+
+r_lgl close_to_zero(r_dbl x, r_dbl tol){
+  return abs(x) <= tol;
 }
 
-inline bool close_to_zero(double x, double tol){
-  return std::fabs(x) < tol;
-}
+r_dbl rel_diff(r_dbl x, r_dbl y, r_dbl scale){
+  r_dbl ax = abs(x);
+  r_dbl ay = abs(y);
+  scale = is_na(scale) ? max(ax, ay) : scale;
 
-double rel_diff(double x, double y, double scale){
-  double ax = std::fabs(x);
-  double ay = std::fabs(y);
-  scale = is_na(scale) ? std::fmax(ax, ay) : scale;
-
-  if (close_to_zero(ax, DEFAULT_TOL) && close_to_zero(ay, DEFAULT_TOL)){
-    return 0.0;
+  if ( (close_to_zero(ax, default_tol()) && close_to_zero(ay, default_tol())).is_true() ) {
+    return r_dbl(0.0);
   } else {
     return abs_diff(x / scale, y / scale);
   }
@@ -55,165 +44,152 @@ double rel_diff(double x, double y, double scale){
 
 // Testing equality
 
-bool equal(double x, double y, double tol){
-  double ax = std::fabs(x);
-  double ay = std::fabs(y);
-  double adiff = abs_diff(x, y);
+r_lgl equal(r_dbl x, r_dbl y, r_dbl tol){
+  r_dbl ax = abs(x);
+  r_dbl ay = abs(y);
+  r_dbl adiff = abs_diff(x, y);
 
   // If any are close to zero use absolute diff, otherwise relative diff
-
-  return ( ax < tol ) || ( ay < tol ) ?
-  adiff < tol :
-    both_same_inf(x, y) ||
-      ( (adiff / std::fmax(ax, ay)) < tol); // Relative diff
+  if ( ((ax <= tol) || (ay <= tol) || any_inf(x, y)).is_true() ) {
+    if (both_same_inf(x, y).is_true()){
+      return r_true;
+   } else {
+    return adiff <= tol;
+   }
+  } else {
+    return (adiff / max(ax, ay)) <= tol;
+  }
 }
 
 // Testing >, >=, < and <=
-bool gt(double x, double y, double tol){
-  double diff = (x - y);
-  bool any_zeros = close_to_zero(x, tol) || close_to_zero(y, tol);
-  if (any_zeros || any_inf(x, y)){
+r_lgl gt(r_dbl x, r_dbl y, r_dbl tol){
+  r_dbl diff = (x - y);
+  r_lgl any_zeros = close_to_zero(x, tol) || close_to_zero(y, tol);
+  if ( (any_zeros || any_inf(x, y)).is_true() ){
+    if (both_same_inf(x, y).is_true()) return r_false;
     return diff > tol;
   } else {
-    return (diff / std::fmax(std::fabs(x), std::fabs(y))) > tol;
+    return (diff / max(abs(x), abs(y))) > tol;
   }
 }
-bool lt(double x, double y, double tol){
-  double diff = (x - y);
-  bool any_zeros = close_to_zero(x, tol) || close_to_zero(y, tol);
-  if (any_zeros || any_inf(x, y)){
+r_lgl lt(r_dbl x, r_dbl y, r_dbl tol){
+  r_dbl diff = (x - y);
+  r_lgl any_zeros = close_to_zero(x, tol) || close_to_zero(y, tol);
+  if ( (any_zeros || any_inf(x, y)).is_true() ){
+    if (both_same_inf(x, y).is_true()) return r_false;
     return diff < -tol;
   } else {
-    return (diff / std::fmax(std::fabs(x), std::fabs(y))) < -tol;
+    return (diff / max(abs(x), abs(y))) < -tol;
   }
 }
-bool gte(double x, double y, double tol){
+r_lgl gte(r_dbl x, r_dbl y, r_dbl tol){
   return gt(x, y, tol) || equal(x, y, tol);
 }
-bool lte(double x, double y, double tol){
+r_lgl lte(r_dbl x, r_dbl y, r_dbl tol){
   return lt(x, y, tol) || equal(x, y, tol);
 }
 
-#define CPPDOUBLES_VECTORISED_COMPARISON(FN)                                      \
-R_xlen_t xn = x.size();                                                           \
-R_xlen_t yn = y.size();                                                           \
-R_xlen_t tn = tolerance.size();                                                   \
-R_xlen_t n = std::max(std::max(xn, yn), tn);                                      \
-if (xn <= 0 || yn <= 0 || tn <= 0){                                               \
-  n = 0;                                                                          \
-}                                                                                 \
-const double *p_x = REAL_RO(x);                                                   \
-const double *p_y = REAL_RO(y);                                                   \
-const double *p_t = REAL_RO(tolerance);                                           \
-writable::logicals out(n);                                                        \
-int* __restrict__ p_out = LOGICAL(out);                                           \
-R_xlen_t i, xi, yi, ti;                                                           \
-for (i = xi = yi = ti = 0; i < n;                                                 \
-xi = (++xi == xn) ? 0 : xi,                                                       \
-  yi = (++yi == yn) ? 0 : yi,                                                     \
-  ti = (++ti == tn) ? 0 : ti, ++i){                                               \
-  p_out[i] = FN(p_x[xi], p_y[yi], p_t[ti]);                                       \
-  if (is_na(p_x[xi]) ||                                                           \
-      is_na(p_y[yi]) ||                                                           \
-      is_na(p_t[ti])){                                                            \
-    p_out[i] = NA_LOGICAL;                                                        \
-  }                                                                               \
-}                                                                                 \
-return out;                                                                       \
+#define CPPDOUBLES_VECTORISED_COMPARISON(FN)                                                \
+r_size_t xn = x.length();                                                                     \
+r_size_t yn = y.length();                                                                     \
+r_size_t tn = tolerance.length();                                                             \
+r_size_t n = std::max(std::max(xn, yn), tn);                                                \
+if (xn <= 0 || yn <= 0 || tn <= 0){                                                         \
+  n = 0;                                                                                    \
+}                                                                                           \
+r_vec<r_lgl> out(n);                                                                        \
+r_size_t i, xi, yi, ti;                                                                     \
+for (i = xi = yi = ti = 0; i < n;                                                           \
+  recycle_index(xi, xn),                                                                    \
+  recycle_index(yi, yn),                                                                    \
+  recycle_index(ti, tn),                                                                    \
+  ++i){                                                                                     \
+  out.set(i, FN(x.get(xi), y.get(yi), tolerance.get(ti)));                                  \
+}                                                                                           \
+return out;
 
 
-[[cpp11::register]]
-logicals cpp_double_equal(doubles x, doubles y, doubles tolerance) {
+[[cppally::register]]
+r_vec<r_lgl> cpp_double_equal(r_vec<r_dbl> x, r_vec<r_dbl> y, r_vec<r_dbl> tolerance) {
   CPPDOUBLES_VECTORISED_COMPARISON(equal)
 }
 
-[[cpp11::register]]
-logicals cpp_double_gt(doubles x, doubles y, doubles tolerance) {
+[[cppally::register]]
+r_vec<r_lgl> cpp_double_gt(r_vec<r_dbl> x, r_vec<r_dbl> y, r_vec<r_dbl> tolerance) {
   CPPDOUBLES_VECTORISED_COMPARISON(gt)
 }
 
-[[cpp11::register]]
-logicals cpp_double_gte(doubles x, doubles y, doubles tolerance) {
+[[cppally::register]]
+r_vec<r_lgl> cpp_double_gte(r_vec<r_dbl> x, r_vec<r_dbl> y, r_vec<r_dbl> tolerance) {
   CPPDOUBLES_VECTORISED_COMPARISON(gte)
 }
 
-[[cpp11::register]]
-logicals cpp_double_lt(doubles x, doubles y, doubles tolerance) {
+[[cppally::register]]
+r_vec<r_lgl> cpp_double_lt(r_vec<r_dbl> x, r_vec<r_dbl> y, r_vec<r_dbl> tolerance) {
   CPPDOUBLES_VECTORISED_COMPARISON(lt)
 }
 
-[[cpp11::register]]
-logicals cpp_double_lte(doubles x, doubles y, doubles tolerance) {
+[[cppally::register]]
+r_vec<r_lgl> cpp_double_lte(r_vec<r_dbl> x, r_vec<r_dbl> y, r_vec<r_dbl> tolerance) {
   CPPDOUBLES_VECTORISED_COMPARISON(lte)
 }
 
-[[cpp11::register]]
-doubles cpp_double_rel_diff(doubles x, doubles y, doubles scale){
-  R_xlen_t xn = x.size();
-  R_xlen_t yn = y.size();
-  R_xlen_t sn = scale.size();
-  R_xlen_t n = std::max(std::max(xn, yn), sn);
+[[cppally::register]]
+r_vec<r_dbl> cpp_double_rel_diff(r_vec<r_dbl> x, r_vec<r_dbl> y, r_vec<r_dbl> scale){
+  r_size_t xn = x.length();
+  r_size_t yn = y.length();
+  r_size_t sn = scale.length();
+  r_size_t n = std::max(std::max(xn, yn), sn);
   if (xn <= 0 || yn <= 0 || sn <= 0){
     n = 0;
   }
-  const double *p_x = REAL_RO(x);
-  const double *p_y = REAL_RO(y);
-  const double *p_s = REAL_RO(scale);
 
-  writable::doubles out(n);
-
-  double* __restrict__ p_out = REAL(out);
-
-  R_xlen_t i, xi, yi, si;
+  r_vec<r_dbl> out(n);
+  r_size_t i, xi, yi, si;
   for (i = xi = yi = si = 0; i < n;
-  xi = (++xi == xn) ? 0 : xi,
-    yi = (++yi == yn) ? 0 : yi,
-    si = (++si == sn) ? 0 : si, ++i){
-    p_out[i] = rel_diff(p_x[xi], p_y[yi], p_s[si]);
+    recycle_index(xi, xn),
+    recycle_index(yi, yn),
+    recycle_index(si, sn),
+    ++i){
+    out.set(i, r_dbl(rel_diff(x.get(xi), y.get(yi), scale.get(si))));
   }
   return out;
 }
 
-[[cpp11::register]]
-logicals cpp_double_all_equal(doubles x, doubles y, doubles tolerance, logicals na_rm) {
-  if (na_rm.size() != 1){
-    stop("`na.rm` must be a logical vector of length 1");
-  }
-  bool skip_na = na_rm[0];
+[[cppally::register]]
+r_lgl cpp_double_all_equal(r_vec<r_dbl> x, r_vec<r_dbl> y, r_vec<r_dbl> tolerance, r_lgl na_rm) {
+  bool skip_na = na_rm.is_true();
   bool has_na;
-  R_xlen_t xn = x.size();
-  R_xlen_t yn = y.size();
-  R_xlen_t tn = tolerance.size();
-  R_xlen_t n = std::max(std::max(xn, yn), tn);
+  r_size_t xn = x.length();
+  r_size_t yn = y.length();
+  r_size_t tn = tolerance.length();
+  r_size_t n = std::max(std::max(xn, yn), tn);
   if (xn <= 0 || yn <= 0 || tn <= 0){
     // Avoid loop if any are length zero vectors
     n = 0;
   }
-  const double *p_x = REAL_RO(x);
-  const double *p_y = REAL_RO(y);
-  const double *p_t = REAL_RO(tolerance);
 
-  writable::logicals out(1);
-  out[0] = true;
+  r_lgl out = r_true;
 
-  R_xlen_t i, xi, yi, ti;
+  r_size_t i, xi, yi, ti;
   for (i = xi = yi = ti = 0; i < n;
-  xi = (++xi == xn) ? 0 : xi,
-    yi = (++yi == yn) ? 0 : yi,
-    ti = (++ti == tn) ? 0 : ti, ++i){
-    has_na = is_na(p_x[xi]) ||
-      is_na(p_y[yi]) ||
-      is_na(p_t[ti]);
+  recycle_index(xi, xn),
+  recycle_index(yi, yn),
+  recycle_index(ti, tn),
+  ++i){
+    has_na = is_na(x.get(xi)) ||
+      is_na(y.get(yi)) ||
+      is_na(tolerance.get(ti));
     if (has_na){
       if (skip_na){
         continue;
       } else {
-        out[0] = NA_LOGICAL;
+        out = na<r_lgl>();
         break;
       }
     }
-    if ( !equal(p_x[xi], p_y[yi], p_t[ti]) ){
-      out[0] = false;
+    if ( equal(x.get(xi), y.get(yi), tolerance.get(ti)).is_false() ){
+      out = r_false;
       break;
     }
   }
